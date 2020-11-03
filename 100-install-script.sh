ed0d710e4e441ff6625d1e5e0288a91a6d96ae35
positive-season <scp94373@gmail.com>
2020-11-03 09:05:04 +0000

    functional scripting

--- 100-install-script.sh
+++ 100-install-script.sh
@@ -1,16 +1,114 @@
 #!/bin/bash
-#Official repos
-sudo pacman -S code
-sudo pacman -S krita
-sudo pacman -S htop
-sudo pacman -S virtualbox
-sudo pacman -S virtualbox-host-dkms
-sudo pacman -S nodejs npm
+#set -e
+#FORKED FROM ARCOLINUX GITHUB REPO
+###############################################################################
+# Author	:	Erik Dubois
+# Website	:	https://www.erikdubois.be
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxb.com
+# Website	:	https://www.arcolinuxiso.com
+# Website	:	https://www.arcolinuxforum.com
+###############################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+###############################################################################
 
+
+###############################################################################
+#
+#   DECLARATION OF FUNCTIONS
+#
+###############################################################################
+
+
+or_install() {
+	if pacman -Qi $1 &> /dev/null; then
+		tput setaf 2
+  		echo "###############################################################################"
+  		echo "################## The package "$1" is already installed"
+      	echo "###############################################################################"
+      	echo
+		tput sgr0
+	else
+    	tput setaf 3
+    	echo "###############################################################################"
+    	echo "##################  Installing package "  $1
+    	echo "###############################################################################"
+    	echo
+    	tput sgr0
+    	sudo pacman -S --noconfirm --needed $1
+    fi
+}
+
+aur_install() {
+	if pacman -Qi $1 &> /dev/null; then
+		tput setaf 2
+  		echo "###############################################################################"
+  		echo "################## The package "$1" is already installed"
+      	echo "###############################################################################"
+      	echo
+		tput sgr0
+	else
+    	tput setaf 3
+    	echo "###############################################################################"
+    	echo "##################  Installing package "  $1
+    	echo "###############################################################################"
+    	echo
+    	tput sgr0
+    	sudo yay -S $1
+    fi
+}
+
+###############################################################################
+#
+#   Official Repos
+#
+###############################################################################
+
+
+ORList=(
+variety
+atom
+code
+krita
+htop
+virtualbox
+nodejs
+npm
+yay
+)
+
+for name in "${ORList[@]}" ; do
+	count=$[count+1]
+	tput setaf 3;echo "Installing package nr.  "$count " " $name;tput sgr0;
+	or_install $name
+done
+
+###############################################################################
+#
+#   AUR
+#
+###############################################################################
 
 
 #yay -S lightdm-slick-greeter
-yay -S postman-bin
-yay -S trello
-yay -S github-desktop-bin
-yay -S spotify-adblock-git
+AURList=(
+postman-bin
+trello
+github-desktop-bin
+spotify-adblock-git
+)
+
+for name in "${AURList[@]}" ; do
+	count=$[count+1]
+	tput setaf 3;echo "Installing package nr.  "$count " " $name;tput sgr0;
+	aur_install $name
+done
+
+
+
+
+
