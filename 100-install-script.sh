5b95cb9fdea4601f165cbc0ed0aa2538a155a61d
positive-season <scp94373@gmail.com>
2020-11-03 07:26:47 +0000

    Initial script

---
+++ 100-install-script.sh
@@ -0,0 +1,15 @@
+#Official repos
+sudo pacman -S code
+sudo pacman -S krita
+sudo pacman -S htop
+sudo pacman -S virtualbox
+sudo pacman -S virtualbox-host-dkms
+
+
+
+
+#yay -S lightdm-slick-greeter
+yay -S postman-bin
+yay -S trello
+yay -S github-desktop-bin
+yay -S spotify-adblock-git
