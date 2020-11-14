d9f5bfc8e5bc09dc2eb374e437deaec4cc55f0f3
positive-season <scp94373@gmail.com>
2020-11-14 22:51:56 +0000

    Updated curors

--- Debian/230-install-bibata-cursors.sh
+++ Debian/230-install-bibata-cursors.sh
@@ -17,8 +17,18 @@ tar -xzvf Bibata-Original-Amber.tar.gz
 tar -xzvf Bibata-Original-Classic.tar.gz
 tar -xzvf Bibata-Original-Ice.tar.gz
 
-echo "Copying all extracted tars to /usr/share/icons"
-sudo cp -r Bibata-*-* /usr/share/icons
+#echo "Copying all extracted tars to /usr/share/icons"
+#sudo cp -r Bibata-*-* /usr/share/icons
+
+echo "Copy all extracted tars to ~/.icons/
+cp -r Bibata-Modern-Amber/ ~/.icons/Bibata-Modern-Amber/
+cp -r Bibata-Modern-Classic/ ~/.icons/Bibata-Modern-Classic/
+cp -r Bibata-Modern-Ice/ ~/.icons/Bibata-Modern-Ice/
+cp -r Bibata-Original-Amber/ ~/.icons/Bibata-Original-Amber/
+cp -r Bibata-Original-Classic/ ~/.icons/Bibata-Original-Classic/
+cp -r Bibata-Original-Ice/ ~/.icons/Bibata-Original-Ice/
+
+gsettings set org.gnome.desktop.interface cursor-theme "$THEMENAME"
 
 read -p "You now have all Bibata cursors, please change under elementary tweaks"
 
