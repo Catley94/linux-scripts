810eb6b4b60a98dd0e420d169ef8794cc044746e
Sam Catley <samuel.catley@social-ability.co.uk>
2021-03-12 10:37:42 +0000

    added work things

---
+++ Work/setup.sh
@@ -0,0 +1,7 @@
+sudo dpkg -i teams_1.4.00.4855_amd64.deb
+sudo apt install krita
+sudo apt install kmail
+#sudo apt install prospect-mail
+#sudo dpkg -i insync_3.3.7.40937-focal_amd64.deb
+snap install discord
+sudo dpkg -i GitHubDesktop-linux-2.6.2-linux1.deb
