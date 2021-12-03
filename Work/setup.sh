28d7102c5b31e4d633263665f39540608cc81c05
Samuel Catley <scp94373@gmail.com>
2021-12-03 15:36:40 +0000

    O365 shortcut, setup creds and added code to snap install

--- Work/setup.sh
+++ Work/setup.sh
@@ -4,4 +4,5 @@ sudo apt install kmail
 #sudo apt install prospect-mail
 #sudo dpkg -i insync_3.3.7.40937-focal_amd64.deb
 snap install discord
+snap install code --classic
 sudo dpkg -i GitHubDesktop-linux-2.6.2-linux1.deb
---
+++ setup-creds.sh
@@ -0,0 +1,2 @@
+git config --global user.email "scp94373@gmail.com"
+git config --global user.name "Samuel Catley"
