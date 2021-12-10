127b05cc9cbac95617cdb244892c217303c8d03b
Samuel Catley <samuel.catley@social-ability.co.uk>
2021-12-10 14:16:34 +0000

    Added vnc

--- Work/setup.sh
+++ Work/setup.sh
@@ -1,9 +1,15 @@
-cp shortcuts/* ~/Desktop
+#cp shortcuts/* ~/Desktop
 sudo dpkg -i teams_1.4.00.4855_amd64.deb
+sudo dpkg -i VNC-Viewer-6.21.1109-Linux-x64.deb
 sudo apt install krita
 sudo apt install kmail
 #sudo apt install prospect-mail
 #sudo dpkg -i insync_3.3.7.40937-focal_amd64.deb
 snap install discord
 snap install code --classic
+snap install spotify
 sudo dpkg -i GitHubDesktop-linux-2.6.2-linux1.deb
+#Adding support for Connectwise executables .jnlp
+sudo apt update
+sudo apt install default-jdk
+sudo apt install icedtea-netx icedtea-plugin
