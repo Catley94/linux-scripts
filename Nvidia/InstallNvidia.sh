31e334dbb19336ab53a841ad80c8ccae68f5f75a
positive-season <scp94373@gmail.com>
2023-03-09 22:21:00 +0000

    Added libnvidia-gl-515

--- Nvidia/InstallNvidia.sh
+++ Nvidia/InstallNvidia.sh
@@ -1,3 +1,3 @@
-sudo apt install -y nvidia-driver-515 nvidia-dkms-515
+sudo apt install -y nvidia-driver-515 nvidia-dkms-515 libnvidia-gl-515
 sudo apt install -y nvidia-settings
 sudo apt install -y nvidia-prime
